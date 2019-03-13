880ffc0b4ce4e88f41889a67c64dacd72f6e2158
Alex Wilmer <djazium@gmail.com>
2019-03-13 11:47:53 -0400

    🏛 classes

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-let socket = new WebSocket('ws://localhost:8080');
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      inputValue: '',
+      username: 'alex',
+      messages: []
+    }
+  }
+  componentDidMount() {
+    console.log('do something here, we mounted!')
+        // Create WebSocket connection.
+    this.socket = new WebSocket('ws://localhost:8080');
 
-function App () {
-  let [inputValue, setInputValue] = useState('')
-  let [username, setUsername] = useState('alex')
-  let [messages, setMessages] = useState([])
-  
-  useEffect(() => {
-    let onMessage = (event) => {
+    // Connection opened
+    this.socket.addEventListener('open', (event) => {
+      // this.socket.send('Hello Server!');
+    });
+
+    // Listen for messages
+    this.socket.addEventListener('message', (event) => {
       let message = JSON.parse(event.data)
       
       if (message.type === 'history') {
-        setMessages(message.data)
+        this.setState({ messages: message.data })
       }
       
       if (message.type === 'single_message') {
-        setMessages([message.data, ...messages])
+        this.setState(state => {
+          return {
+            messages: [message.data, ...state.messages]
+          }
+        })
       }
-    }
-    
-    socket.addEventListener('message', onMessage)
-    
-    return () => {
-      socket.removeEventListener('message', onMessage)
-    }
-  }, [messages])
+    });
+  }
   
-  return (
-    <div className="App">
-      <input 
-        onChange={event => 
-          setUsername(event.target.value)
-        } 
-        value={username}
-      />
-      <input 
-        onChange={event => {
-          setInputValue(event.target.value)
-          socket.send(JSON.stringify({
-            message: event.target.value,
-            username,
-          }))
-        }}
-        value={inputValue}
-      />
-      <hr />
-      {messages.map((msg, i) => 
-        <div key={i}>
-          <span>{`${new Date(msg.date)}`} - </span>
-          <b>{msg.username}: </b>
-          <span>{msg.message}</span>
-        </div>
-      )}
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <input 
+          onChange={event => 
+            this.setState({ username: event.target.value })
+          } 
+          value={this.state.username}
+        />
+        <input 
+          onChange={event => {
+            this.setState({ inputValue: event.target.value })
+            this.socket.send(JSON.stringify({
+              message: event.target.value,
+              username: this.state.username,
+            }))
+          }}
+          value={this.state.inputValue}
+        />
+        <hr />
+        {this.state.messages.map((msg, i) => 
+          <div key={i}>
+            <span>{`${new Date(msg.date)}`} - </span>
+            <b>{msg.username}: </b>
+            <span>{msg.message}</span>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-    
+
 export default App;
